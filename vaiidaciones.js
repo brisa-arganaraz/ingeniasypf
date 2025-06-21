1c7b2af83b56b743b3b74ea1a60dacc310092dd1
¨Estrellagit config --global user.email estreyani@gmail.com¨ <Estreyani@gmail.com>
2025-06-21 10:30:11 -0300

    Se crean login.html y registro.html - Se agregan imágenes: icono de usuario y fondo para registro - Se aplica estilo CSS a la página de registro - Se modifica index.html

---
+++ vaiidaciones.js
@@ -0,0 +1,20 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const forms = document.querySelectorAll(".needs-validation");
+
+  forms.forEach((form) => {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+});
